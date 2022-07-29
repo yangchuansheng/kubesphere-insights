7e5e15a91b109c935ff9f7e245f08055fbd40dc3
Carson Yang <yangchuansheng33@gmail.com>
2022-07-29 15:45:56 +0800

    Create ks_all_contributors.sh

---
+++ ks_all_contributors.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for i in $(cat ks_repo.txt);
+do
+  for j in {1..30};
+  do
+      curl -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/$i/contributors?per_page=100&page=$j"|jq -r '.[].login'|sort|uniq >> ~/ks_all_contributors_source_$(date +%Y%m%d-%H%M%S).txt
+  done
+done
+
+cat ks_all_contributors_source.txt|grep -Ev "\-bot|\[bot\]|^null$"|sort|uniq > ks_all_contributors_$(date +%Y%m%d-%H%M%S).txt

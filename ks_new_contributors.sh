fa1c5281ff3a169e5f3b92885e9d54178d9a0d77
Carson Yang <yangchuansheng33@gmail.com>
2022-08-19 16:15:11 +0800

    Update ks_new_contributors.sh

--- ks_new_contributors.sh
+++ ks_new_contributors.sh
@@ -9,9 +9,9 @@ for i in $(cat ks_repo.txt);
 do
   for j in {1..30};
   do
-      curl -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/$i/commits?since=${FROM_DATE}T00:00:00Z&until=${UNTIL_DATE}T00:00:00Z&per_page=100&page=$j"|jq -r '.[].committer.login'|sort|uniq >> ~/ks_new_contributors_source.txt
-      curl -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/$i/commits?since=${FROM_DATE}T00:00:00Z&until=${UNTIL_DATE}T00:00:00Z&per_page=100&page=$j"|jq -r '.[].author.login'|sort|uniq >> ~/ks_new_contributors_source.txt
+      curl -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/$i/commits?since=${FROM_DATE}T00:00:00Z&until=${UNTIL_DATE}T00:00:00Z&per_page=100&page=$j"|jq -r '.[].committer.login'|sort|uniq >> ~/ks_all_contributors_within_two_weeks_source.txt
+      curl -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/$i/commits?since=${FROM_DATE}T00:00:00Z&until=${UNTIL_DATE}T00:00:00Z&per_page=100&page=$j"|jq -r '.[].author.login'|sort|uniq >> ~/ks_all_contributors_within_two_weeks_source.txt
   done
 done
 
-cat ks_new_contributors_source.txt|grep -Ev "\-bot|\[bot\]|^null$"|sort|uniq > ks_new_contributors_${DATE}.txt
+cat ks_all_contributors_within_two_weeks_source.txt|grep -Ev "\-bot|\[bot\]|^null$"|sort|uniq > ks_all_contributors_within_two_weeks_${DATE}.txt
